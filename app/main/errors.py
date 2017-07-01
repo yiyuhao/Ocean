c03dd941831cbd50523d45e35fb2f5610f7b0411
_GL0oMY <uestcyyh@sina.com>
2017-07-01 20:30:18 +0800

    error handler
    yiyuhao
    2017年7月1日 20:29:55

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,1 +1,12 @@
 from . import main
+from flask import render_template
+
+
+@main.app_errorhandler(404)
+def page_not_find(e):
+    return render_template('errors/404.html'), 404
+
+
+@main.app_errorhandler(500)
+def interval_server_error(e):
+    return render_template('errors/500.html'), 500
