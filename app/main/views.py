d45f9650545dd3beea950ae837c915b7f158655f
_GL0oMY <uestcyyh@sina.com>
2017-07-03 23:35:14 +0800

    搞定了个人资料界面的前端
    yiyuhao
    2017年7月3日 23:34:22

--- app/main/views.py
+++ app/main/views.py
@@ -11,5 +11,7 @@ def index():
 
 @main.route('/user/<user_name>')
 def profile(user_name):
-    if not User.query.filter_by(user_name=user_name).first():
+    user = User.query.filter_by(user_name=user_name).first()
+    if user is None:
         abort(404)
+    return render_template('user/profile.html', user=user)
