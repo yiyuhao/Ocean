d2555abbb51b1bcb9bc4ebc924e53cb4b790f8ac
_GL0oMY <uestcyyh@sina.com>
2017-08-05 13:10:32 +0800

    新增：代码覆盖检测
    yiyuhao
    2017年8月5日 13:10:29

--- ocean.py
+++ ocean.py
@@ -4,6 +4,14 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 import os
 
+
+COV = None
+if os.environ.get('OCEAN_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+
 app = create_app(os.getenv('OCEAN_ENVIRONMENT') or 'default')
 
 manager = Manager(app)
@@ -17,11 +25,27 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """单元测试"""
+    if coverage and not os.environ.get('OCEAN_COVERAGE'):
+        import sys
+        os.environ['OCEAN_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+        print('代码覆盖统计:')
+        COV.report()
+        base_dir = os.path.abspath(os.path.dirname(__file__))
+        cov_dir = os.path.join(base_dir, 'tmp/coverage')
+        COV.html_report(directory=cov_dir)
+        print('HTML: file://{}/index.html'.format(cov_dir))
+        COV.erase()
 
 
 @manager.command
