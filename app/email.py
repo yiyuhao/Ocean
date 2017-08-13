7b76489e32c2470a44b0a967c9dc113475b321ab
_GL0oMY <uestcyyh@sina.com>
2017-08-13 22:14:56 +0800

    fix: celery邮件
    yiyuhao
    2017年8月13日 22:14:53

--- app/email.py
+++ app/email.py
@@ -5,7 +5,7 @@ from . import celery, mail
 
 
 # 开发环境使用线程发送邮件
-def send_async_email(app, msg):
+def multi_thread_send_mail(app, msg):
     # flask-mail的send()使用current_app，所以在不同线程执行mail.send()时，必须人工创建上下文
     with app.app_context():
         mail.send(msg)
@@ -13,8 +13,16 @@ def send_async_email(app, msg):
 
 # 生产环境使用celery
 @celery.task
-def multi_thread_send_mail(msg):
-    mail.send(msg)
+def send_async_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(subject='{head} - {subject}'.format(head=app.config['MAIL_SUBJECT_PREFIX'], subject=subject),
+                  recipients=[to],
+                  body=render_template('{}.txt'.format(template), **kwargs),
+                  html=render_template('{}.html'.format(template), **kwargs),
+                  sender=app.config['MAIL_SENDER'])
+    # flask-mail的send()使用current_app，所以在不同线程执行mail.send()时，必须人工创建上下文
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
@@ -26,7 +34,7 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['MAIL_SENDER'])
     # 生产环境使用celery
     if app.config['CELERY_BROKER_URL']:
-        multi_thread_send_mail.delay(msg)
+        send_async_email.delay(to, subject, template, **kwargs)
     else:
-        th = Thread(target=send_async_email, name='send email', args=[app, msg])
+        th = Thread(target=multi_thread_send_mail, name='send email', args=[app, msg])
         th.start()
