b060b83c373aa6ce3669ee3665f9d3db70459673
_GL0oMY <uestcyyh@sina.com>
2017-08-13 23:07:41 +0800

    fix: celery邮件
    yiyuhao
    2017年8月13日 23:07:38

--- app/email.py
+++ app/email.py
@@ -12,7 +12,13 @@ def send_async_email(app, msg):
 
 
 @celery.task
-def celery_send_mail(msg):
+def celery_send_mail(to, subject, template):
+    app = current_app._get_current_object()
+    msg = Message(subject='{head} - {subject}'.format(head=app.config['MAIL_SUBJECT_PREFIX'], subject=subject),
+                  recipients=[to],
+                  body=render_template('{}.txt'.format(template), **kwargs),
+                  html=render_template('{}.html'.format(template), **kwargs),
+                  sender=app.config['MAIL_SENDER'])
     mail.send(msg)
 
 
@@ -25,7 +31,7 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['MAIL_SENDER'])
     # 生产环境使用celery
     if app.config['CELERY_BROKER_URL']:
-        celery_send_mail.delay(msg)
+        celery_send_mail.delay(to, subject, template)
     else:
         th = Thread(target=send_async_email, name='send email', args=[app, msg])
         th.start()
