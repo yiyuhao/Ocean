e396235280405401b31720712debc61390aa2f3d
_GL0oMY <uestcyyh@sina.com>
2017-08-13 21:50:30 +0800

    fix: 创建2个函数分别作用于生产环境的celery发送，与开发环境的多线程发送邮件
    yiyuhao
    2017年8月13日 21:50:27

--- app/email.py
+++ app/email.py
@@ -4,12 +4,17 @@ from flask_mail import Message
 from . import celery, mail
 
 
-@celery.task
+# 开发环境使用线程发送邮件
 def send_async_email(app, msg):
     # flask-mail的send()使用current_app，所以在不同线程执行mail.send()时，必须人工创建上下文
-    # 使用celery可不用创建上下文
     with app.app_context():
         mail.send(msg)
+
+
+# 生产环境使用celery
+@celery.task
+def multi_thread_send_mail(msg):
+    mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
@@ -21,7 +26,7 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['MAIL_SENDER'])
     # 生产环境使用celery
     if app.config['CELERY_BROKER_URL']:
-        send_async_email.delay(app, msg)
+        multi_thread_send_mail.delay(msg)
     else:
         th = Thread(target=send_async_email, name='send email', args=[app, msg])
         th.start()
