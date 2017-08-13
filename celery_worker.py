1fc4d4db5aae7a0af4771bd8d3320a1206356ae2
_GL0oMY <uestcyyh@sina.com>
2017-08-13 11:27:25 +0800

    fix: 启动celery时还未导入环境变量，导致默认以rabbitMQ作为broker启动
    yiyuhao
    2017年8月13日 11:27:21

--- celery_worker.py
+++ celery_worker.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 import os
+
+# 从.env文件中导入环境变量
+if os.path.exists('environment_var.env'):
+    print('Importing environment from .env...')
+    for line in open('environment_var.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+    print('Imported env var')
+
 from app import celery, create_app
 
 app = create_app(os.getenv('OCEAN_ENVIRONMENT') or 'default')
